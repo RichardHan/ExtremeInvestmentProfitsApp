a900ecf8b04ccef087f5c4d2ee37dc82f2bb21c1
RichardHan <qwweee7467@gmail.com>
2016-07-09 01:22:59 +0800

    split app.js and parse_app.js

--- ExtremeInvestmentProfitsApp/app.js
+++ ExtremeInvestmentProfitsApp/app.js
@@ -3,6 +3,7 @@
     url = require('url'),
     cheerio = require('cheerio'),
     crawlerSetup = require('./crawlerSetup.js'),
+    indexBuilder = require('./indexBuilder.js'),
     fs = require('fs');
 
 var crawler = new Crawler(crawlerSetup.hostName, crawlerSetup.initialPath);
@@ -18,6 +19,9 @@ crawler.domainWhitelist = crawlerSetup.domainWhitelist;
 
 crawler.cache = new Crawler.cache('D:/tmp');
 
+// discover Html Link Rules
+//   must contain html
+//  must without   #comments and  #?cid=
 crawler.discoverResources = function (buffer, queueItem) {
     var $ = cheerio.load(buffer.toString("utf8"));
     return $("a[href]").map(function () {
@@ -29,37 +33,22 @@ crawler.discoverResources = function (buffer, queueItem) {
 };
 
 crawler.on("fetchcomplete", function (queueItem, responseBuffer, response) {
-
-
     if (queueItem.url.indexOf(".html") > 0
         && queueItem.url.indexOf("index.html") == -1
         && queueItem.url.indexOf("about.html") == -1
         && queueItem.url.indexOf("archives.html") == -1) {
+
         var $ = cheerio.load(responseBuffer.toString("utf8"));
         var title = $('.date-header').text();
         title = title.replace(/\//g, '_');
         title = title.split('_')[2] + "_" + title.split('_')[0] + "_" + title.split('_')[1];
-
         console.log("%s (%d bytes) %s %s ", queueItem.url, responseBuffer.length, response.headers['content-type'], title);
 
         var html = $('.entry').html();
-        var html = "<h1>" + queueItem.url + "</h1>" + html;
+        var html = "<h1 class='url'>" + queueItem.url + "</h1>" + html;
+        var html = "<h1 class='date-header'>" + $('.date-header').text() + "</h1>" + html;
 
-        var _10kStr = $($('strong')[0]).text();
-        var _10k = _10kStr.substr(_10kStr.lastIndexOf('$')).replace('>', '').replace(/,/g,'');
-        var _20kStr = $($('strong')[1]).text();
-        var _20k = _20kStr.substr(_20kStr.lastIndexOf('$')).replace('>', '').replace(/,/g,'');
-
-        var writeLine = _10k + "," + _20k + "," + queueItem.url + "," + title + "," + responseBuffer.length;
-
-        fs.appendFile('D:/tmp/index.csv', writeLine + '\r\n', function (err) {
-            if (err) {
-                fs.appendFile('D:/tmp/error.txt', queueItem.url + '\r\n', function (err) {
-                    return;
-                });
-                return console.log(err);
-            }
-        });
+        //indexBuilder(responseBuffer.toString("utf8"));
 
         fs.writeFile("D:/tmp/" + title + ".html", html, function (err) {
             if (err) {
---
+++ ExtremeInvestmentProfitsApp/parse_app.js
@@ -0,0 +1,28 @@
+﻿var Crawler = require("simplecrawler"),
+    request = require("request"),
+    url = require('url'),
+    cheerio = require('cheerio'),
+    indexBuilder = require('./IndexBuilder.js'),
+    fs = require('fs');
+
+fs.readdir("D:/tmp", function (err, files) {
+    if (!err)
+        for (var i in files) {
+            if (files[i].indexOf(".html") > 0) {
+                console.log(files[i]);
+                //var buffer = fs.readFile('D:/tmp' + files[i]);
+                var buffer = fs.readFile('D:/tmp/' + files[i], function (err, data) {
+                    if (err) throw err;
+                    indexBuilder(data.toString("utf8"));
+                    //console.log(data);
+                });
+            }
+        }
+    else
+        throw err;
+});
+
+function BuildIndexFile() {
+
+
+}
